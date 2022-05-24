43e6c4d914c79cbe81b1a567d881263830887cdb
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-24 21:19:24 +0430

    add filepath & save in database

--- modules/CreatePost.ts
+++ modules/CreatePost.ts
@@ -6,7 +6,7 @@ import multer from "multer";
 import fs, { mkdir } from "fs";
 
 const app = express();
-
+// app.use(express.static("upload"));
 const getDirImage = () => {
   const year = new Date().getFullYear();
   const month = new Date().getMonth() + 1;
@@ -25,10 +25,12 @@ const fileStorage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     // save in database
-    const filepath: any = `${process.env.PORT}/${getDirImage}/${file.originalname}`;
+    const filepath: any = `${getDirImage()}/${file.originalname}`;
 
     // if (!fs.existsSync(filepath)) {
     cb(null, file.originalname);
+
+    req.body.filepath = filepath;
     // } else {
     // cb(null, `${Date.now()} - ${file.originalname}`);
     // }
@@ -37,60 +39,60 @@ const fileStorage = multer.diskStorage({
 
 export const upload = multer({
   storage: fileStorage,
-  limits: { fileSize: 1024 * 1024 * 10 },
+  limits: { fileSize: 1024 * 1024 * 5 },
 }).single("file");
 
 console.log(upload);
 export const createPost = async (req: Request, res: Response) => {
   let createdPost: any = [];
-  const { title, content } = req.body;
+  const { title, content, filepath } = req.body;
+  console.log(req.body);
 
   try {
-    // const newPost = await prisma.post.create({
-    //   data: {
-    //     title: title,
-    //     content: content,
-    //     image: "",
-    //     author: {
-    //       //@ts-ignore
-    //       connect: { id: req.userId },
-    //     },
-    //   },
-    // });
-    // console.log(newPost);
-    // if (newPost) {
-    //   res.status(200).json({ path, filename });
-    // } else {
-    //   res.status(400).json("error");
-    // }
-
-    // const newpost = await prisma.post.create({
-    //   data: {
-    //     title: "Post 3",
-    //     content: "alsp boop!",
-    //     image: filepath,
-    //     author: {
-    //       connectOrCreate: {
-    //         create: {
-    //           email: "someone@example.com",
-    //           password: "someone",
-    //         },
-    //         where: {
-    //           email: "someone@example.com",
-    //         },
-    //       },
-    //     },
-    //   },
-    // });
-    // console.dir({ newpost }, { depth: Infinity });
-    // console.log(newpost);
-    // const post = await prisma.post.findMany({});
-
-    // console.log(post);
-    // createdPost.push({ ...newpost });
+    const newPost = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        image: filepath,
+        author: {
+          //@ts-ignore
+          connect: { id: req.userId },
+        },
+      },
+    });
+    console.log(newPost);
+    if (newPost) {
+      res.status(200).json({});
+    } else {
+      res.status(400).json("error");
+    }
 
     res.status(200).json({});
   } catch (error) {
     res.status(500).json(error);
   }
 };
+// const newpost = await prisma.post.create({
+//   data: {
+//     title: "Post 3",
+//     content: "alsp boop!",
+//     image: filepath,
+//     author: {
+//       connectOrCreate: {
+//         create: {
+//           email: "someone@example.com",
+//           password: "someone",
+//         },
+//         where: {
+//           email: "someone@example.com",
+//         },
+//       },
+//     },
+//   },
+// });
+// console.dir({ newpost }, { depth: Infinity });
+// console.log(newpost);
+// const post = await prisma.post.findMany({});
+
+// console.log(post);
+// createdPost.push({ ...newpost });
