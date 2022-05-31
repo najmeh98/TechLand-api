38bd027986a70e0f27c5b92573a36e3146b6e3ce
najmeh98 <foroughi.najmeh98@gmail.com>
2022-05-31 14:55:42 +0430

    add post request

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { Authentication, Login } from "./modules/auth";
-import { CreatePost, upload } from "./modules/CreatePost";
 // import fileUpload from "express-fileupload";
 import multer from "multer";
 import { auth } from "./utilis/authenticate";
+import { createPost, upload } from "./modules/CreatePost";
 
 dotenv.config();
 const app = express();
@@ -19,8 +19,16 @@ const port = 7000;
 
 app.get("/", (req, res) => {
   console.log("already Done!");
-  res.send("Hello World!");
+  res.json("Hello World!");
 });
+
+app.post("/api/data/add", (req, res) => {
+  console.log("req.file:", req.file);
+  console.log("req.body", req.body);
+  let body = req.file;
+  res.status(200).json({ body });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
@@ -32,7 +40,46 @@ app.post("/api/user/login", Login);
 app.post("/api/user/auth", Authentication);
 
 // Create Post
-app.post("/api/data/add-post", upload, CreatePost);
+app.post("/api/data/add-post", createPost);
 
 //get Post
-app.get("/api/data/get-post");
+app.get("/api/data/getAllpost/:id");
+
+// const spacesEndpoint = new AWS.Endpoint(
+//   "https://s3.ir-thr-at1.arvanstorage.com"
+// );
+// const accesskeyid: any = process.env.UPLOADER_ACCESS_KEYID;
+// const secretkey: any = process.env.UPLOADER_SECRET_ACCESSKEY;
+// const s3 = new AWS.S3({
+//   endpoint: spacesEndpoint,
+//   accessKeyId: accesskeyid,
+//   secretAccessKey: secretkey,
+// });
+
+// export const uploadService = async (
+//   req: any,
+//   bucket: any,
+//   format: any
+// ): Promise<string> => {
+//   const date: any = Date();
+//   const hashkey: any = process.env.UPLEADER_HASH_KEY;
+//   const hashalg: any = process.env.HASH_ALGORITHM;
+//   const filename: any = createHmac(hashalg, hashkey).update(date).digest("hex");
+//   if (!req.files) {
+//     const answer: string = "noimg";
+//     return answer;
+//   } else {
+//     const boody: string = req.files.file.data;
+//     let base64data = Buffer.from(boody, "binary");
+//     const uploadParams = await s3
+//       .upload({
+//         Bucket: bucket,
+//         Body: base64data,
+//         Key: filename + format,
+//         ACL: "private",
+//       })
+//       .promise();
+//     const answer: string = uploadParams.Location;
+//     return answer;
+//   }
+// };
