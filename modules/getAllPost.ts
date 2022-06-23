0e614e71e511daeb3b25b25ef056ea390bff2798
najmeh98 <foroughi.najmeh98@gmail.com>
2022-06-23 15:04:45 +0430

    get all post with post router

--- modules/getAllPost.ts
+++ modules/getAllPost.ts
@@ -1,12 +1,35 @@
 import { prisma } from "../utilis/prisma";
 import { Request, Response } from "express";
 
-export const getAllPost = async (req: Request, res: Response) => {
-  const { id } = req.params;
+export const GetAllPost = async (req: Request, res: Response) => {
+  const { id } = req.body;
+
   // try {
   console.log(id);
-  res.send("hello world");
+
+  const posts = await prisma.user.findFirst({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      //@ts-ignore
+      post: true,
+    },
+  });
+
+  if (posts) {
+    res.status(200).json(posts);
+  } else {
+    res.status(400).json("No post found");
+  }
   // } catch (error) {
-  // console.log(error);
+  //   res.status(500).json({ error });
   // }
 };
+
+/**
+ * TODO
+ * - add get router for get all posts
+ * -
+ * -
+ */
