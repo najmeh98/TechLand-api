3671ed07c70cfabc1dac7284c9104254e7cd19fa
najmeh98 <foroughi.najmeh98@gmail.com>
2022-07-08 00:07:34 +0430

    update user auth

--- utilis/authenticate.ts
+++ utilis/authenticate.ts
@@ -3,18 +3,17 @@ import { Request, Response, NextFunction } from "express";
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   const token: any = req.header("authorization");
+  const jwttoken: any = process.env.JWT_TOKEN;
   try {
-    if (process.env.JWT_TOKEN) {
-      const tokenUserId = jwt.verify(token, process.env.JWT_TOKEN);
+    const tokenUserId = jwt.verify(token, jwttoken);
 
-      console.log("tokenUserId", tokenUserId);
+    console.log("tokenUserId", tokenUserId);
 
-      //@ts-ignore
-      req.userId = tokenUserId.userId;
-      //@ts-ignore
-      console.log(req.userId);
-      return next();
-    }
+    //@ts-ignore
+    req.userId = tokenUserId.userId;
+    //@ts-ignore
+    console.log(req.userId);
+    return next();
   } catch (error) {
     res.status(403).send("error from authorization");
   }
