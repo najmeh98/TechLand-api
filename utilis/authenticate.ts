5b76c60e4d274c49de592de4f7ea9654f418ea5d
najmeh98 <foroughi.najmeh98@gmail.com>
2022-07-08 13:02:31 +0430

    verify token

--- utilis/authenticate.ts
+++ utilis/authenticate.ts
@@ -4,17 +4,17 @@ import { Request, Response, NextFunction } from "express";
 export const auth = (req: Request, res: Response, next: NextFunction) => {
   const token: any = req.header("authorization");
   const jwttoken: any = process.env.JWT_TOKEN;
+
   try {
     const tokenUserId = jwt.verify(token, jwttoken);
 
     console.log("tokenUserId", tokenUserId);
 
     //@ts-ignore
-    req.userId = tokenUserId.userId;
-    //@ts-ignore
-    console.log(req.userId);
+    req.userId = tokenUserId;
+
     return next();
   } catch (error) {
-    res.status(403).send("error from authorization");
+    res.status(500).json(error);
   }
 };
