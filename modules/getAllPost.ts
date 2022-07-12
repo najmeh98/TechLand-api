696d42f9adaa395d1ee1f798886fbea774d0e157
najmeh98 <foroughi.najmeh98@gmail.com>
2022-07-12 23:05:17 +0430

    update post router

--- modules/getAllPost.ts
+++ modules/getAllPost.ts
@@ -5,39 +5,39 @@ export const GetAllPost = async (req: Request, res: Response) => {
   const id: number = req.body.id;
 
   // const userid: number = req.userId;
-  // try {
-  //@ts-ignore
-  console.log("userid", req.userId);
+  try {
+    //@ts-ignore
+    console.log("userid", req.userId);
 
-  const posts = await prisma.user.findFirst({
-    where: {
-      // id: Number(id),
-      //@ts-ignore
-      id: req.userId,
-    },
-    include: {
-      //@ts-ignore
-      post: true,
-    },
-  });
+    const posts = await prisma.user.findFirst({
+      where: {
+        id: Number(id),
+        // //@ts-ignore
+        // id: req.userId,
+      },
+      include: {
+        //@ts-ignore
+        post: true,
+      },
+    });
 
-  if (posts) {
-    const data: any = {
-      fullName: posts.name,
-      email: posts.email,
-      bio: posts.bio,
-      id: posts.id,
-      skill: posts.skill,
-      post: posts.post,
-    };
+    if (posts) {
+      const data: any = {
+        fullName: posts.name,
+        email: posts.email,
+        bio: posts.bio,
+        id: posts.id,
+        skill: posts.skill,
+        post: posts.post,
+      };
 
-    res.status(200).json(data);
-  } else {
-    res.status(400).json("No post found");
+      res.status(200).json(data);
+    } else {
+      res.status(400).json("No post found");
+    }
+  } catch (error) {
+    res.status(500).json({ error });
   }
-  // } catch (error) {
-  //   res.status(500).json({ error });
-  // }
 };
 
 /**
