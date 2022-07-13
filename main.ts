8e2c6d6d2a7e990c8aa9ed288f11ef402b0ed53f
najmeh98 <foroughi.najmeh98@gmail.com>
2022-07-13 19:15:44 +0430

    Change the project structure

--- main.ts
+++ main.ts
@@ -1,22 +1,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Authentication, Login } from "./modules/auth";
+import { Authentication, Login } from "./modules/user/auth";
 // import fileUpload from "express-fileupload";
 import multer from "multer";
 import { auth } from "./utilis/authenticate";
 import jwt from "jsonwebtoken";
 import { Request, Response } from "express";
-import { GetAllPost } from "./modules/getAllPost";
+import { GetAllPost } from "./modules/user/post/getAllPost";
 import fileUpload from "express-fileupload";
-import { DeletePost } from "./modules/deletePost";
+import { DeletePost } from "./modules/user/post/deletePost";
 import { prisma } from "./utilis/prisma";
-import { EditPost } from "./modules/editPost";
-import { CreatePost } from "./modules/createPost";
+import { EditPost } from "./modules/user/post/editPost";
+import { CreatePost } from "./modules/user/post/createPost";
 import crypto from "crypto";
-import { userInfo } from "./modules/userInfo";
-import { getPost } from "./modules/getPost";
-import { userValid } from "./modules/userValid";
+import { userInfo } from "./modules/user/users/userInfo";
+import { getPost } from "./modules/user/post/getPost";
+import { userValid } from "./modules/user/users/userValid";
+import { register } from "./modules/admin/adminauth";
 
 dotenv.config();
 const app = express();
@@ -44,85 +45,29 @@ app.get("/", (req, res) => {
   res.json("Hello World!");
 });
 
-app.delete("/api/data/deletePost/:id", auth, DeletePost);
+// user
 
+app.delete("/api/data/deletePost/:id", auth, DeletePost);
 // Login
 app.post("/api/user/login", Login);
-
 // Authorization
 app.post("/api/user/auth", Authentication);
-
 // Create Post
 app.post("/api/data/add-post", auth, CreatePost);
-
 //Edit Post
 app.post("/api/data/editPost/:id", auth, EditPost);
 //get Post
 app.post("/api/data/getAllpost/:id", auth, GetAllPost);
-
 //get user
 app.post("/api/data/getUser/:id/:slug", auth, userInfo);
-
 //get post
 app.get("/api/data/getPost/:id", auth, getPost);
-
 // user valid
 app.post("/api/data/userValid", auth, userValid);
 
-app.get("/", (req, res) => {
-  const secret = "GfG";
-  const hash: any = crypto
-    .createHmac("sha256", secret)
-    .update("GeeksforGeeks")
-    .digest("hex");
-  if (hash) {
-    console.log(hash);
-  } else {
-    console.log("does not excited");
-  }
+//admin
 
-  console.log("already Done!");
-  res.json({ hash });
-});
-
-// const spacesEndpoint = new AWS.Endpoint(
-//   "https://s3.ir-thr-at1.arvanstorage.com"
-// );
-// const accesskeyid: any = process.env.UPLOADER_ACCESS_KEYID;
-// const secretkey: any = process.env.UPLOADER_SECRET_ACCESSKEY;
-// const s3 = new AWS.S3({
-//   endpoint: spacesEndpoint,
-//   accessKeyId: accesskeyid,
-//   secretAccessKey: secretkey,
-// });
-
-// export const uploadService = async (
-//   req: any,
-//   bucket: any,
-//   format: any
-// ): Promise<string> => {
-//   const date: any = Date();
-//   const hashkey: any = process.env.UPLEADER_HASH_KEY;
-//   const hashalg: any = process.env.HASH_ALGORITHM;
-//   const filename: any = createHmac(hashalg, hashkey).update(date).digest("hex");
-//   if (!req.files) {
-//     const answer: string = "noimg";
-//     return answer;
-//   } else {
-//     const boody: string = req.files.file.data;
-//     let base64data = Buffer.from(boody, "binary");
-//     const uploadParams = await s3
-//       .upload({
-//         Bucket: bucket,
-//         Body: base64data,
-//         Key: filename + format,
-//         ACL: "private",
-//       })
-//       .promise();
-//     const answer: string = uploadParams.Location;
-//     return answer;
-//   }
-// };
+app.post("/api/admin/auth", register);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
