4f1a7c28b5e2960e3d635fc33b80239bdbc8c1e0
najmeh98 <foroughi.najemh98@gmail.com>
2022-07-30 16:08:01 +0430

    update  user router

--- modules/admin/users/getAllusers.ts
+++ modules/admin/users/getAllusers.ts
@@ -1,22 +1,32 @@
 import { Request, Response } from "express";
 import { prisma } from "../../../utilis/prisma";
 
-export const getAllusers = async (
+export const GetAllusers = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const id: any = req.body.id;
+  // const id: any = req.body.id;
 
   try {
-    console.log(id);
-
-    const result = await prisma.user.findMany({});
-
+    const result = await prisma.user.findMany({
+      select: {
+        name: true,
+        email: true,
+        phoneNumber: true,
+        family: true,
+        createdAt: true,
+        id: true,
+        address: true,
+        username: true,
+      },
+    });
     if (result) {
+      console.log("result", result);
       res.status(200).json(result);
     } else {
       res.status(400).json(Error);
     }
+    console.log("hi");
   } catch (error) {
     res.status(500).json(error);
   }
