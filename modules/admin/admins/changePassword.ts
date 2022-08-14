719c113b8bb9167606600dc9906d4d6c8c193e4e
najmeh98 <foroughi.najmeh98@gmail.com>
2022-08-14 23:04:12 +0430

    Change password for admins

---
+++ modules/admin/admins/changePassword.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express";
+import { hashpassmethod } from "../../../utilis/authenticate";
+import { prisma } from "../../../utilis/prisma";
+
+export const changePassword = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  console.log("query", req.query);
+  console.log("bdy", req.body);
+
+  const passsword: string = req.body?.adminPassword?.currentpassword;
+  const newpass: string = req.body?.adminPassword?.newpassword;
+
+  const id: any = req.query.query;
+
+  if (!passsword || !newpass || passsword.length == null) {
+    res.status(401).json("data problem");
+  }
+  try {
+    const findUser = await prisma.admin.findFirst({
+      where: {
+        //@ts-ignore
+        id: req.userId,
+      },
+    });
+    if (findUser) {
+      console.log(findUser);
+
+      const pass: string = findUser?.password;
+      console.log(" db pass", pass);
+      // hash pass
+      const hashCurrentPass: string = hashpassmethod(passsword);
+      console.log("hash pass", hashCurrentPass);
+
+      console.log(hashCurrentPass === pass);
+
+      // hash new pass
+      const hashNewPass: string = hashpassmethod(newpass);
+      console.log("hash new pass", hashNewPass);
+
+      const id: any = findUser?.id;
+
+      if (pass === hashCurrentPass) {
+        const updatepass = await prisma.admin.update({
+          where: {
+            id: Number(id),
+          },
+          data: {
+            password: hashNewPass,
+          },
+        });
+        if (updatepass) {
+          console.log(updatepass);
+          res.status(200).json("update password");
+        } else {
+          res.status(400).json("error");
+        }
+      } else {
+        console.log(Error);
+        res.status(404).json("The password is wrong");
+      }
+    } else {
+      res.status(402).json("user not found");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
