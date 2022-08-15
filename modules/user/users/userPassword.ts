5dad25c4a4ee9ea387336a6feacd1526b4da6654
najmeh98 <foroughi.najmeh98@gmail.com>
2022-08-15 09:50:16 +0430

    Add change password for users

---
+++ modules/user/users/userPassword.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import { hashpassmethod } from "../../../utilis/authenticate";
+import { prisma } from "../../../utilis/prisma";
+
+export const userPassword = async (req: Request, res: Response) => {
+  const currentpass: string = req.body?.passwordInfo?.currentPassword;
+
+  const newpass: string = req.body?.passwordInfo?.newPassword;
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        //@ts-ignore
+        id: userId,
+      },
+    });
+
+    if (user) {
+      const id: any = user?.id;
+      const oldpass: string = user?.password;
+
+      const hashCurrentPass: string = hashpassmethod(currentpass);
+
+      const hashNewPass: string = hashpassmethod(newpass);
+
+      if (oldpass === hashCurrentPass) {
+        const updatePass = await prisma.user.update({
+          where: {
+            id: Number(id),
+          },
+          data: {
+            password: hashNewPass,
+          },
+        });
+
+        if (updatePass) {
+          res.status(200).json(updatePass);
+        } else {
+          res.status(400).json(Error);
+        }
+      } else {
+        console.log(Error);
+        res.status(404).json("The password is wrong");
+      }
+    } else {
+      res.status(401).json("user not found");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
