8336ff0f14c1685a27d2faf57e6466a84d2ca2c4
najmeh98 <foroughi.najmeh98@gmail.com>
2022-08-19 00:21:31 +0430

    Update router create admin

--- modules/admin/admins/adminCreate.ts
+++ modules/admin/admins/adminCreate.ts
@@ -9,8 +9,6 @@ export const adminCreate = async (
 ): Promise<void> => {
   const dt: any = req.body?.adminInfo;
   const token: string = req.body.token;
-  console.log(dt);
-  console.log("token", token);
 
   try {
     const value = dataValidation(dt);
@@ -18,14 +16,13 @@ export const adminCreate = async (
     if (value == false) {
       res.status(401).json("data problem");
     }
-    console.log(req);
-    console.log("body", req.body);
 
     // password hash
     const pass: string = passwordHash(dt.password);
-    console.log(pass);
+
     const admin = await prisma.admin.create({
       data: {
+        id: undefined,
         name: dt.name,
         family: dt.family,
         email: dt.email,
@@ -33,24 +30,25 @@ export const adminCreate = async (
         address: dt.address,
         phoneNumber: dt.phoneNumber,
         createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
         token: "0",
         password: pass,
+        bio: "",
+        job: "",
+      },
+      select: {
+        name: true,
+        family: true,
+        username: true,
+        email: true,
+        address: true,
+        phoneNumber: true,
+        createdAt: true,
       },
     });
 
-    const newadmin = {
-      name: admin?.name,
-      family: admin?.family,
-      username: admin?.username,
-      email: admin?.email,
-      address: admin?.address,
-      phoneNmuber: admin?.phoneNumber,
-      createdAt: admin?.createdAt,
-    };
-    console.log(admin);
-
     if (admin) {
-      res.status(200).json(newadmin);
+      res.status(200).json(admin);
     } else {
       res.status(400).json("error");
     }
