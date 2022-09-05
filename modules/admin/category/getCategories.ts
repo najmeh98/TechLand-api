9db42892faf9bf1ebf295012e3b4de7acb453342
najmeh98 <foroughi.najmeh98@gmail.com>
2022-09-05 23:25:01 +0430

    get all category

---
+++ modules/admin/category/getCategories.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { prisma } from "../../../utilis/prisma";
+
+export const getCategories = async (req: Request, res: Response) => {
+  try {
+    const getCategory = await prisma.category.findMany({
+      select: {
+        name: true,
+        description: true,
+        id: true,
+        image: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (getCategory) {
+      console.log(getCategories);
+      res.status(200).json(getCategory);
+    } else {
+      res.status(400).json(Error);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
