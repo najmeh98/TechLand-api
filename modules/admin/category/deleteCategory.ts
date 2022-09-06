5f4d305e265ac4e528f3f2510122cfebc4e83336
najmeh98 <foroughi.najmeh98@gmail.com>
2022-09-06 23:41:03 +0430

    Added delete category route

---
+++ modules/admin/category/deleteCategory.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { prisma } from "../../../utilis/prisma";
+
+export const deleteCategory = async (req: Request, res: Response) => {
+  console.log("req", req);
+  console.log("body", req.body);
+  console.log("catId", req.query);
+
+  try {
+    const catId: any = req.body?.wbId;
+    const deleteCate = await prisma.category.delete({
+      where: {
+        id: catId,
+      },
+    });
+    if (deleteCate) {
+      res.status(200).json(deleteCate);
+    } else {
+      res.status(400).json(Error);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
