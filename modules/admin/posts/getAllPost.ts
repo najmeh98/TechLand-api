dc3eb52a5d345987b2fd2c2de277ff15288af975
najmeh98 <foroughi.najmeh98@gmail.com>
2022-09-10 23:48:39 +0430

    Add router to receive all posts

---
+++ modules/admin/posts/getAllPost.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../../../utilis/prisma";
+import { Request, Response } from "express";
+
+export const GetAllPost = async (req: Request, res: Response) => {
+  //@ts-ignore
+  const adminId = req.adminId;
+
+  try {
+    const adminPost = await prisma.admin.findFirst({
+      where: {
+        id: adminId,
+      },
+      select: {
+        post: {
+          select: {
+            title: true,
+            content: true,
+            image: true,
+            createdAt: true,
+            updatedAt: true,
+            id: true,
+            categoryId: true,
+          },
+        },
+      },
+    });
+
+    if (adminPost) {
+      res.status(200).json(adminPost);
+    } else {
+      res.status(400).json("No post found");
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+/**
+ * TODO
+ * - add get router for get all posts
+ * -
+ * -
+ */
