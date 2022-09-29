a3d49050e089fcd097aa6bc166ed8be69319aecb
najmeh98 <foroughi.najmeh98@gmail.com>
2022-09-29 13:21:28 +0330

    Create a router for post data

---
+++ modules/user/getPostInfo.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import { prisma } from "../../utilis/prisma";
+
+export const getPostInfo = async (req: Request, res: Response) => {
+  console.log("req", req);
+
+  const psId: string = req?.query?.query as string;
+  try {
+    const post = await prisma.post.findFirst({
+      where: {
+        id: psId,
+      },
+      select: {
+        image: true,
+        title: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+        admin: {
+          select: {
+            family: true,
+            name: true,
+            image: true,
+            id: true,
+            job: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(400).json(Error);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
